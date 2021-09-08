15fe4eac9c11185f8a37964a57b6739f09c5e2b4
Tin Lai <oscar@tinyiu.com>
2021-09-09 08:13:42 +1000

    highlight info line and wait for process end before exiting with error code
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- run_all.py
+++ run_all.py
@@ -6,25 +6,29 @@ import time
 import glob
 
 container = "fireflyiii-spectre"
+script_path = os.path.dirname(os.path.realpath(__file__))
 
 
 # start container
 p = subprocess.Popen(["docker", "start", container])
 p.wait()
 if p.poll() != 0:
-    print(f"failed to start container {container}")
+    print(f"> failed to start container {container}")
     exit(1)
 
 
-command = ["docker", "exec", "-it", container, "php", "artisan", "importer:import"]
+command = ["docker", "exec", container, "php", "artisan", "importer:import"]
 
-files = [os.path.split(p)[-1] for p in glob.glob('configs/*.json')]
+files = [os.path.split(p)[-1] for p in glob.glob(f'{script_path}/configs/*.json')]
 
 processes = set()
 max_processes = 6
 
 for name in files:
-    processes.add(subprocess.Popen([*command, f"/configs/{name}"]))
+    processes.add(subprocess.Popen([*command, f"/configs/{name}"],
+#        stdout=subprocess.DEVNULL,
+#        stderr=subprocess.STDOUT,
+        ))
     if len(processes) >= max_processes:
         os.wait()
         processes.difference_update([p for p in processes if p.poll() is not None])
@@ -40,6 +44,8 @@ for f, p in zip(files, processes):
 
 # stop container
 p = subprocess.Popen(["docker", "stop", container])
+p.wait()
 if p.poll() != 0:
-    print(f"failed to stop container {container}")
+    print(f"> failed to stop container {container}")
     exit(1)
+
